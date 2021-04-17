82b710270b85240872b02ec83edf21aa8b6d319a
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2021-04-17 19:01:13 +0200

    Initial commit

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,7 @@
 Start-Transcript -Path 'C:/terraform-log.txt' -append;
 $VerbosePreference = 'Continue';
 $InformationPreference = 'Continue';
+Install-WindowsFeature -name Web-Server -IncludeManagementTools;
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force;
 Install-Module -Name Az -Repository PSGallery -Force;
 Stop-Transcript;
